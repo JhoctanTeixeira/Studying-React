09212dec6cfecf7f5b017b1ab1172be7c0b3d692
Jhoctan21 <rm88206@fiap.com.br>
2021-08-13 18:08:25 -0300

    Exercício 2 Finalizado

---
+++ exercicio02/src/components/corpo/Corpo.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Treino from'../img/exerF.jpg'
+
+export default function Corpo() {
+
+   return(
+      <>
+      <div className="c-corpo">
+      <img className="t-image_treino" src={Treino} alt="Exercicios se arrume e bora treinar em CASA!!!!" />
+      <h2 className="t-title">Exercicios recomendados</h2>
+      <p>1 - Polichinelo completo Em pé, com as pernas unidas e os braços estendidos ao lado do corpo, dê um salto para cima. Ao mesmo tempo, afaste os pés lateralmente e bata as mãos acima da cabeça. Volte para a posição inicial e depois agache, tocando as mãos no solo. Estenda as pernas e repita o exercício... </p>
+      <p>2 - Agachamento com deslocamento lateral Em pé, fique com as pernas unidas e coloque as mãos na cintura. Dê um passo para o lado. Estenda os braços à frente, contraia o abdome e agache até suas coxas ficarem paralelas ao chão. Volte para a posição inicial e execute o movimento para o outro lado....</p>
+      <p>3 - Prancha com toque nas pernas Fique em posição de flexão de braços, com as mãos afastadas em uma largura igual a dos ombros e o abdome contraído, para estabilizar o corpo. Leve a mão esquerda até a lateral da coxa de mesmo lado. Volte e repita com a outra mão....</p>
+      <p>4 - Salto com afundo Fique em pé, com as pernas afastadas na largura do quadril e as mãos na cintura. Flexione levemente os joelhos para pegar impulso e dê um salto para cima. Depois, dê um pequeno salto, coloque a perna direita à frente e flexione os joelhos, fazendo um afundo. Retorne à posição inicial e repita o movimento, agora colocando a perna esquerda à frente....</p>
+      </div>
+      </>
+)
+}
---
+++ exercicio02/src/components/footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Footer() {
+
+   return(
+      <>
+      <div className="f-footer">
+        <footer >
+            <p className="t-title">Autor: Jhoctan Teixeira</p>
+            <p><a href="mailto:rm88206@fiap.com.br">rm88206@fiap.com.br</a></p>
+        </footer>
+        </div>
+      </>
+)
+}
