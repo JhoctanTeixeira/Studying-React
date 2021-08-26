36c12e65224991d06bc81dd0d566b337bbfb7d06
Jhoctan21 <rm88206@fiap.com.br>
2021-08-26 09:24:07 -0300

    Aula 3

--- exercicio02/src/components/caixateste/CaixaTest.js
+++ exercicio02/src/components/caixateste/CaixaTest.js
@@ -1,25 +1,21 @@
 import React, {useState}  from 'react'
 import './CaixaTest.css'
-import Adicao from '../adicao/Adicao'
 
 export default function CaixaTest(props) {
 
-    const [valorState, setvalorState] = useState(10)
+    const [caixaState, setvalorState] = useState(10)
     let valorVariavel = 10;
-
+    <button></button>
+    
     const aumentar = () =>{
-        setvalorState(valorState + 10)
+        setvalorState(caixaState + 10)
         valorVariavel += 10
     }
 
    return(
       <>
       <div>
-          <p className="v-valor1">valorState : {valorState}</p>
-          <p className="v-valor1">Valor da Variavel : {valorVariavel}</p>
-          <button className='btn'  onClick={()=> aumentar()}   >Aumentar em 10</button>
-
-          <Adicao resultado2={valorState} operacao2={aumentar}/>
+         
       </div>
 
       </>
